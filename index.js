e4cef754d206ab7636d5019cd3208717e0b5883a
Michał Sajnóg <michal.sajnog@hotmail.com>
2017-03-08 01:18:02 +0100

    Remove isDeveloping flag

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
   name: 'ember-component-respo',
-  
-  isDevelopingAddon: function() {
-    return true;
-  },
 };
